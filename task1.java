b62d27baac1b7bf0b0658bb94ee6e04f7d909d0f
Alex <mr.tal44@yandex.ru>
2023-04-17 15:53:45 +0300

    HomeWork task 1

--- task1.java
+++ task1.java
@@ -1,6 +1,19 @@
 public class task1 {
     public static void main(String[] args) {
-        String s = "              2";
-        System.out.println(s);
+        int [][] arr = new int [3][5];
+        for (int i = 0; i < arr.length; i++){
+            System.out.println();
+            for (int j = 0; j < arr[i].length;j++){
+                System.out.printf("%d", arr[i][j]);
+                
+            }
+        }
+        System.out.println();
+        
+
     }
+
+    static String getType(Object o){
+    return o.getClass().getSimpleName();
+}
 }
