eddcab0dccc1aa64300865a70f6bb986f7370931
Alex44Rus <mr.tal44@yandex.ru>
2023-06-15 15:04:13 +0300

    OOPSem5hwtask1

---
+++ OOPSeminar5/task1/Main.java
@@ -0,0 +1,11 @@
+package OOPSeminar5.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorView view = new CalculatorView();
+        CalculatorModel model = new CalculatorModel();
+        CalculatorPresenter presenter = new CalculatorPresenter(view, model);
+
+        presenter.performCalculation();
+    }
+}
