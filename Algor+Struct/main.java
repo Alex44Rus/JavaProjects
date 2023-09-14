42b71d22c01463d5444e716b76462d7aaf21ffb0
Alex44Rus <116281102+Alex44Rus@users.noreply.github.com>
2023-09-14 22:27:47 +0300

    hw4

---
+++ Algor+Struct/main.java
@@ -0,0 +1,24 @@
+package Algorithmes.HomeWork.hw4;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(7);
+        tree.insert(6);
+        tree.insert(8);
+        tree.print();
+
+
+
+    }
+}
+
+
